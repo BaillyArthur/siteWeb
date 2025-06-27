d1813890d4bc48e472a3779fdac27f56edb22236
Q220169 <ar.bailly@student.helmo.be>
2025-06-27 22:20:56 +0200

    ajout de la bare de navigation et de l'image de fond

--- MonSite/Program.cs
+++ MonSite/Program.cs
@@ -1,3 +1,4 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,19 +6,19 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions());
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
--- MonSite/obj/Debug/net8.0/MonSite.AssemblyInfo.cs
+++ MonSite/obj/Debug/net8.0/MonSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cc1326743d9f2cacc7280f0b9d74cfc82380a7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
