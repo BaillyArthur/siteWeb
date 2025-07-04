55cc5485d486dbc9e6d2362697187c7986365c20
Q220169 <ar.bailly@student.helmo.be>
2025-07-04 13:48:39 +0200

    ajout de about me + db + debut page projet

---
+++ MonSite/Pages/Project.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MonSite.Pages;
+
+public class ProjectModel : PageModel
+{
+    
+    public int? IdProject { get; set; }
+
+    public void OnGet(int? idProject)
+    {
+        IdProject = idProject;
+    }
+    
+}
